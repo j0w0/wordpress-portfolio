d0c0c412dea422d4358459e9e7e988179fd9e049
j0shuaryan <josh@j0w0.com>
2018-07-06 17:25:28 -0500

    home page update

--- wp-content/themes/j0w0/index.php
+++ wp-content/themes/j0w0/index.php
@@ -3,10 +3,11 @@
     <section class="section-padding">
         <div class="container-fluid container-max-width">
             
-            <div id="work-intro" class="text-center">
+            <div id="work-intro" class="text-center post-container">
                 <h2 class="heading-3d-primary">My Work</h2>
-                <p>I specialize in web/graphic design and web development.<br />
-                I create graphics using Adobe Creative Cloud. I create responsive websites using WordPress, HTML5, CSS3, and jQuery.</p>
+                <p>I design and develop interactive, data-driven websites and web applications.</p>
+                <p>I received my BFA in Communication Design, but found my way into the realm of web design and development. My process includes designing in Illustrator and then transforming that design into a responsive website using HTML, CSS, jQuery/Javascript, and Wordpress.</p>
+                <p>Among these web projects, my portfolio also consists of my past graphic design work.</p>
             </div>
             
             <?php get_template_part( 'template-parts/portfolio/items', 'home' ); ?>
